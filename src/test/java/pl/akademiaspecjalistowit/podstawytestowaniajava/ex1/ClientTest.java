4acd6dd00ba63dba75fbdf6ec2f17430934cf09c
przemyslawgesieniecasit <przemyslaw.gesieniec@gmail.com>
2024-02-16 18:42:10 +0100

    init

---
+++ src/test/java/pl/akademiaspecjalistowit/podstawytestowaniajava/ex1/ClientTest.java
@@ -0,0 +1,11 @@
+package pl.akademiaspecjalistowit.podstawytestowaniajava.ex1;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ClientTest {
+
+    public void test(){
+
+    }
+
+}
